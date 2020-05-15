60c69d7ba4938ac5daa8e8f72adb908538e7f01e
Saurav Singh <33387479+saurav2401@users.noreply.github.com>
2020-05-15 16:58:00 +0530

    Create day_14.py

---
+++ week2/day_14.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        final = 0
+        n = len(shift)
+        for i in range(n):
+            if shift[i][0] == 0:
+                final -= shift[i][1]
+            if shift[i][0] == 1:
+                final += shift[i][1]
+        final %= n
+            
+        if final == 0:
+            return s
+        return s[-final:] + s[:-final]
